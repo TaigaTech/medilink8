3fa0886e8c5e668913a22b5ec716bf9ac6f34168
Ivan Magwara <ivanmagwara291@gmail.com>
2025-08-28 00:48:51 +0300

    preparing for deployment

---
+++ app/layout.js
@@ -0,0 +1,46 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Header from "@/components/header";
+import { dark } from "@clerk/themes";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "MediLink - Doctor's Appointment Platform",
+  description: "Connect with doctors anytime, anywhere",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={{ 
+      baseTheme: dark,
+       }}
+       >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+             {/* header */}
+             <Header />
+
+      <main className="min-h-screen">{children}</main>
+
+        {/*footer */}
+        <footer className="bg-muted/50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-200">
+            <p>This platform was funded by Rwenzururu Center for Innovation and Development and it was developed by Safe Haven Digital Services Ltd.</p>
+          </div>
+        </footer>
+          </ThemeProvider>
+        </body>
+   </html>
+   </ClerkProvider>
+  );
+}
